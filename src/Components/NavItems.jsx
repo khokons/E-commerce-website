cbd642e2802714f82365e1765ebcc9a23a94caa0
khokons <khokon.web9226@gmail.com>
2024-09-10 20:53:59 +0600

    Showcase our product section done

--- src/Components/NavItems.jsx
+++ src/Components/NavItems.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import logo from '../assets/images/logo/logo.png'
+import logo from '../assets/images/logo/logo.png';
+import logo2 from '../assets/images/logo/logo-3.png';
+
 
 const NavItems = () => {
 
--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,10 +1,15 @@
 import Banner from "./Banner";
+import CategoryShowcase from "./CategoryShowcase";
+import HomeCategory from "./HomeCategory";
 
 
 const Home = () => {
     return (
         <div>
            <Banner></Banner>
+           <HomeCategory></HomeCategory>
+           <CategoryShowcase></CategoryShowcase>
+         
         </div>
     );
 };
